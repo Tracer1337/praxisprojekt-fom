f5ea6f4a72d527320ca6d77d1a43c616dfc0b4e5
Tracer1337 <merlin.moelter@gmail.com>
2023-05-05 08:12:04 +0200

    Persistiere YOLOv5s Training im Repository

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/Tracer1337/praxisprojekt-fom.git
 cd praxisprojekt-fom/yolov5/
-python train.py --img 640 --epochs 3 --data gtsdb.yaml --weights yolov5s.pt
+python train.py --img 1280 --epochs 300 --batch-size 4 --data gtsdb.yaml --weights yolov5s.pt

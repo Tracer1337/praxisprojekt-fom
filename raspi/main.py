b08eb4dcc8bdefbdbf652210f14563a36203f1c8
Tracer1337 <merlin.moelter@gmail.com>
2023-05-07 13:31:11 +0200

    Benenne object_detection zu road_sign_detection um

--- raspi/main.py
+++ raspi/main.py
@@ -7,22 +7,23 @@ from message_announcer import MessageAnnouncer
 from threading import Thread
 
 vid = cv2.VideoCapture(0)
-object_stream = MessageAnnouncer()
+
+road_sign_stream = MessageAnnouncer()
 
 def parse_args():
   global args
   parser = argparse.ArgumentParser(description='SunFounder PiCar-V Remote Control')
-  parser.add_argument('--object-detection-url', help='url to the object-detection endpoint', required=True)
+  parser.add_argument('--road-sign-detection-url', help='url to the road-sign-detection endpoint', required=True)
   return parser.parse_args()
 
-def detect(img, args):
+def detect_road_signs(img, args):
   res = requests.post(
-    args.object_detection_url,
+    args.road_sign_detection_url,
     files={ 'image': img },
   )
 
   if res.status_code != 200:
-    raise Exception('Failed to call object-detection endpoint')
+    raise Exception('Failed to call road-sign-detection endpoint')
   
   return res.json()
 
@@ -32,20 +33,20 @@ def format_sse(data, event=None):
         msg = f'event: {event}\n{msg}'
     return msg
 
-def run_object_detection(args):
-  print("Run Object Detection")
+def run_road_sign_detection(args):
+  print("Run Road Sign Detection")
   while True:
     _, img = vid.read()
     frame = cv2.imencode('.jpg', img)[1].tobytes()
-    objects = detect(frame, args)
-    data = format_sse(json.dumps(objects))
-    object_stream.announce(data)
+    detections = detect_road_signs(frame, args)
+    data = format_sse(json.dumps(detections))
+    road_sign_stream.announce(data)
 
 app = Flask(__name__)
-@app.route('/object-stream')
+@app.route('/road-sign-stream')
 def index():
   def stream():
-    messages = object_stream.listen()
+    messages = road_sign_stream.listen()
     while True:
       msg = messages.get()
       yield msg
@@ -58,7 +59,7 @@ def start_server():
 if __name__ == '__main__':
   args = parse_args()
 
-  thread = Thread(target=run_object_detection, args=(args,))
+  thread = Thread(target=run_road_sign_detection, args=(args,))
   thread.start()
 
   start_server()

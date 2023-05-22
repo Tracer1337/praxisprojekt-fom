c4e29b7789da512a1349bc0d4c211a2243d7b7c8
Tracer1337 <merlin.moelter@gmail.com>
2023-05-22 16:28:35 +0200

    Verbessere Layout für Mobilgeräte

--- raspi/controller/src/components/Controller/Controller.tsx
+++ raspi/controller/src/components/Controller/Controller.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Stack, ToggleButton } from "@mui/material";
+import { CircularProgress, Stack, ToggleButton, Box } from "@mui/material";
 import useControllerState from "./hooks/useControllerState";
 import Joystick from "./Joystick";
 import MemoryIcon from "@mui/icons-material/Memory";
@@ -19,7 +19,48 @@ function Controller() {
     return <CircularProgress />;
   }
 
-  const buttons = (
+  const automationProps = {
+    value: "automation",
+    selected: state.automation,
+    onChange: () => setAutomation(!state.automation),
+  };
+
+  const speedProps = {
+    value: "speed",
+    selected: state.speed,
+    onChange: () => setSpeed(!state.speed),
+  };
+
+  if (isMobile) {
+    return (
+      <Stack
+        direction="row"
+        spacing={3}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+          "& .MuiToggleButton-root, & .MuiToggleButton-root.Mui-selected": {
+            backgroundColor: "white !important",
+          },
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+          <Joystick size={130} throttle={100} onChange={handleMove} />
+          <ToggleButton {...automationProps} sx={{ ml: 4 }}>
+            <MemoryIcon />
+          </ToggleButton>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+          <ToggleButton {...speedProps} sx={{ mr: 4 }}>
+            <SpeedIcon />
+          </ToggleButton>
+          <Joystick size={130} throttle={100} onChange={handleCamera} />
+        </Box>
+      </Stack>
+    );
+  }
+
+  return (
     <Stack
       direction="row"
       sx={{
@@ -28,39 +69,14 @@ function Controller() {
         },
       }}
     >
-      <ToggleButton
-        value="automation"
-        selected={state.automation}
-        onChange={() => setAutomation(!state.automation)}
-        sx={{ mr: 4 }}
-      >
+      <ToggleButton {...automationProps} sx={{ mr: 4 }}>
         <MemoryIcon />
       </ToggleButton>
-      <ToggleButton
-        value="speed"
-        selected={state.speed}
-        onChange={() => setSpeed(!state.speed)}
-      >
+      <ToggleButton {...speedProps}>
         <SpeedIcon />
       </ToggleButton>
     </Stack>
   );
-
-  if (isMobile) {
-    return (
-      <Stack
-        direction="row"
-        spacing={3}
-        sx={{ justifyContent: "space-between", alignItems: "flex-end" }}
-      >
-        <Joystick size={130} throttle={100} onChange={handleMove} />
-        {buttons}
-        <Joystick size={130} throttle={100} onChange={handleCamera} />
-      </Stack>
-    );
-  }
-
-  return buttons;
 }
 
 export default Controller;
--- raspi/controller/src/views/MainView/MobileLayout.tsx
+++ raspi/controller/src/views/MainView/MobileLayout.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { useRaspiConfig } from "../../lib/raspi";
+import { WEBCAM_HEIGHT, WEBCAM_WIDTH, useRaspiConfig } from "../../lib/raspi";
 import ImageWithObjects from "./ImageWithObjects";
 import useTrafficSignDetection from "./hooks/useTrafficSignDetection";
 import Controller from "../../components/Controller";
@@ -17,55 +17,64 @@ function MobileLayout() {
         height: "100vh",
         width: "100vw",
         backgroundColor: "black",
+        position: "relative",
         display: "flex",
         justifyContent: "center",
       }}
     >
       <Box
         sx={{
-          position: "relative",
           height: "100vh",
-          aspectRatio: 4 / 3,
-          "& > .MuiBox-root:first-of-type": {
-            height: "100vh",
-            overflow: "hidden",
-            img: {
-              width: "100%",
-            },
-          },
+          aspectRatio: WEBCAM_WIDTH / WEBCAM_HEIGHT,
+          position: "relative",
         }}
       >
-        <ImageWithObjects
-          src={videoStreamUrl}
-          objects={trafficSignDetections}
-        />
+        <Box
+          sx={{
+            "& > .MuiBox-root:first-of-type": {
+              overflow: "hidden",
+              img: {
+                maxHeight: "100%",
+                maxWidth: "100%",
+              },
+            },
+          }}
+        >
+          <ImageWithObjects
+            src={videoStreamUrl}
+            objects={trafficSignDetections}
+          />
+        </Box>
         <Box
           sx={{
             position: "absolute",
             inset: 0,
+            bottom: "unset",
+            height: "40%",
             display: "flex",
-            flexDirection: "column",
             justifyContent: "space-between",
-            m: 2,
+            m: 1,
           }}
         >
-          <Box
-            sx={{
-              height: "40%",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Action />
-            <ObjectList
-              objects={trafficSignDetections}
-              getImageUrl={getTrafficSignUrl}
-            />
-          </Box>
-          <Box>
-            <Controller />
-          </Box>
+          <Action />
+          <ObjectList
+            objects={trafficSignDetections}
+            getImageUrl={getTrafficSignUrl}
+          />
         </Box>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          inset: 0,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          mx: 8,
+          my: 2,
+        }}
+      >
+        <Controller />
       </Box>
     </Box>
   );
